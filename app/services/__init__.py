f37f47e519a833f9e0ff94ded06d7eaaa1aab0d5
TheusGeto <matheusdbrwolf2021@gmail.com>
2024-11-13 20:14:01 +0000

    FINALIZADO

--- app/services/__init__.py
+++
@@ -1,7 +0,0 @@
-from models.usuario_model import Usuario
-from sqlalchemy.orm import Session
-
-class UsuarioRepository: 
-    def init (self, session: Session): 
-        self.session = session
-    def salvar_usuario(self, usuario: Usuario): self.session.add(usuario) self.session.commit()
